73785b2749252458ed09c6e83d2458d65a18d222
Ariel Paixão dos Santos <arielpaixao10@gmail.com>
2019-06-03 16:33:09 -0300

    removing api key

--- firebase/manha/senai-svigufo-react-firebase/src/services/firebaseConfig.js
+++ firebase/manha/senai-svigufo-react-firebase/src/services/firebaseConfig.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyB-We2pM_Ip4TUC5CdsElHtBuSURHW-XCc",
-    authDomain: "svigufomanhafernando.firebaseapp.com",
-    databaseURL: "https://svigufomanhafernando.firebaseio.com",
-    projectId: "svigufomanhafernando",
-    storageBucket: "svigufomanhafernando.appspot.com",
-    messagingSenderId: "923048413744",
-    appId: "1:923048413744:web:920f26bc0b1dbe44"
+    //Firebase Configurations, get it from the Firebase Dashboard.
 };
 
 firebase.initializeApp(config);
